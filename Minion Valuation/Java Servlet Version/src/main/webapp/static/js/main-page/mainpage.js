d96d35d14c5a0fd865a0ec8c9edcb8e0c224de92
OneWayDream <onewaydream@mail.ru>
2021-06-19 14:04:19 +0300

    Minion Valuation (Servlet Edition) upload

---
+++ Minion Valuation/Java Servlet Version/src/main/webapp/static/js/main-page/mainpage.js
@@ -0,0 +1,10 @@
+window.document.onreadystatechange=function() {
+    change_background();
+    minion_image();
+    fuel_image();
+    upgrade1_image();
+    upgrade2_image();
+    set_values_mainpage();
+    page_move();
+    startTooltip();
+};
